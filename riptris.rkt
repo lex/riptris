a812a8d68ce3c1387332545766a04b9b77f4f398
lex <lex@users.noreply.github.com>
2017-04-01 21:17:54 +0300

    use blocks

--- riptris.rkt
+++ riptris.rkt
@@ -20,42 +20,51 @@
 (define BLOCK-EDGE-RADIUS -0.12)
 (define BLOCK-BORDER-WIDTH 0.5)
 
+(define BOARD-WIDTH 10)
+(define BOARD-HEIGHT 20)
+
 (define frame (new frame%
                    [label "riptris"]
                    [width WIDTH]
                    [height HEIGHT]))
+
+(struct block (color))
 
 (define (draw-block dc x y color)
   (send dc set-brush color 'solid)
   (send dc set-pen COLOR-BLOCK-EDGE BLOCK-BORDER-WIDTH 'hilite)
   (send dc draw-rounded-rectangle x y BLOCK-WIDTH BLOCK-HEIGHT BLOCK-EDGE-RADIUS))
 
-(define BOARD-WIDTH 10)
-(define BOARD-HEIGHT 20)
-
 ;; (define fallen-blocks (make-list BOARD-HEIGHT (make-vector BOARD-WIDTH 0)))
 (define fallen-blocks
   (list
-   #(0 0 0 0 0 0 0 0 0 0)
-   #(0 0 0 0 0 0 0 0 0 0)
-   #(0 0 0 0 0 0 0 0 0 0)
-   #(0 0 0 0 0 0 0 0 0 0)
-   #(0 0 0 0 0 0 0 0 0 0)
-   #(0 0 0 0 0 0 0 0 0 0)
-   #(0 0 0 0 0 0 0 0 0 0)
-   #(0 0 0 0 0 0 0 0 0 0)
-   #(0 0 0 0 0 0 0 0 0 0)
-   #(0 0 0 0 0 0 0 0 0 0)
-   #(0 0 0 0 0 0 0 0 0 0)
-   #(0 0 0 0 0 0 0 0 0 0)
-   #(0 0 0 0 0 0 0 0 0 0)
-   #(0 0 0 0 0 0 0 0 0 0)
-   #(0 0 0 0 0 0 0 0 0 0)
-   #(0 0 0 0 0 0 0 0 0 0)
-   #(0 0 0 0 0 0 1 0 0 0)
-   #(0 0 0 1 0 0 1 0 0 0)
-   #(0 1 0 1 0 0 1 1 0 1)
-   #(1 1 1 1 1 0 1 0 1 0)))
+   (vector 0 0 0 0 0 0 0 0 0 0)
+   (vector 0 0 0 0 0 0 0 0 0 0)
+   (vector 0 0 0 0 0 0 0 0 0 0)
+   (vector 0 0 0 0 0 0 0 0 0 0)
+   (vector 0 0 0 0 0 0 0 0 0 0)
+   (vector 0 0 0 0 0 0 0 0 0 0)
+   (vector 0 0 0 0 0 0 0 0 0 0)
+   (vector 0 0 0 0 0 0 0 0 0 0)
+   (vector 0 0 0 0 0 0 0 0 0 0)
+   (vector 0 0 0 0 0 0 0 0 0 0)
+   (vector 0 0 0 0 0 0 0 0 0 0)
+   (vector 0 0 0 0 0 0 0 0 0 0)
+   (vector 0 0 0 0 0 0 0 0 0 0)
+   (vector 0 0 0 0 0 0 0 0 0 0)
+   (vector 0 0 0 0 0 0 0 0 0 0)
+   (vector 0 0 0 0 0 0 0 0 0 0)
+   (vector 0 0 0 0 0 0 1 0 0 0)
+   (vector 0 0 0 1 0 0 1 0 0 0)
+   (vector 0 1 0 1 0 0 1 1 0 1)
+   (vector 1 1 1 1 1 0 1 0 1 0)))
+
+;; convert ones to orange blocks
+(for-each
+ (lambda (row)
+   (for ([i (in-range 0 (vector-length row))])
+     (when (equal? (vector-ref row i) 1)
+       (vector-set! row i (block COLOR-ORANGE))))) fallen-blocks)
 
 (define (draw-game dc fallen)
   ;; draw borders
@@ -63,8 +72,8 @@
   (define row-length (vector-length (list-ref fallen 0)))
 
   (for ([x (in-range 0 (+ row-length 2))])
-      (draw-block dc (* x BLOCK-WIDTH) 0 COLOR-GAME-BORDER)
-      (draw-block dc (* x BLOCK-WIDTH) (* (+ (length fallen) 1) BLOCK-HEIGHT) COLOR-GAME-BORDER))
+    (draw-block dc (* x BLOCK-WIDTH) 0 COLOR-GAME-BORDER)
+    (draw-block dc (* x BLOCK-WIDTH) (* (+ (length fallen) 1) BLOCK-HEIGHT) COLOR-GAME-BORDER))
 
   (for ([y (in-range 1 (+ (length fallen) 1))])
     (draw-block dc 0 (* y BLOCK-HEIGHT) COLOR-GAME-BORDER)
@@ -75,8 +84,12 @@
     (define row (list-ref fallen y))
 
     (for ([x (in-range 0 (vector-length row))])
-      (when (eq? (vector-ref row x) 1)
-        (draw-block dc (* (+ x 1) BLOCK-WIDTH) (* (+ y 1) BLOCK-WIDTH) COLOR-ORANGE)))))
+      (define potential-block (vector-ref row x))
+      (when (block? potential-block)
+        (define drawing-x (* (+ x 1) BLOCK-WIDTH))
+        (define drawing-y (* (+ y 1) BLOCK-WIDTH))
+        (define drawing-color (block-color potential-block))
+        (draw-block dc drawing-x drawing-y drawing-color)))))
 
 (new canvas% [parent frame]
      [paint-callback
